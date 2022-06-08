8bea4f362bb7a3c68b07ee31438da28d3b2f444f
Christina Lewis <clewis13@ufl.edu>
2022-06-08 18:53:01 -0400

    coded game reset

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var previousWord = document.getElementById('previous-word')
-//previousWord.textContent = wordToGuess
-var incorrectLetters = document.getElementById('incorrect-letters')
+//declare variables
 var remainingGuesses = document.getElementById('remaining-guesses') 
-var wins = document.getElementById('wins') 
+var incorrectLetters = document.getElementById('incorrect-letters')
+var wins = document.getElementById('wins')
 var losses = document.getElementById('losses')
-
-
+var previousWord = document.getElementById('previous-word')
 
 var correct = 0
 var incorrect = 0
+var win = 0;
+var loss = 0;
 
 var words = [
   'bananas',
@@ -26,53 +26,91 @@ var words = [
 
 
 
-/*Select a word at random from "words" array*/
+//Select a word at random from "words" array
 var random_index = Math.floor(Math.random() * words.length);
 var randomWord = words[random_index];
 
 
-/*place it in word to guess element w/ letter replaced with underscores*/
+//place it in word to guess element w/ letter replaced with underscores
 
 var wordToGuess = document.querySelector('h2')
-var currentWord = (randomWord.replace(/[a-z]/g, '_'))
-wordToGuess.textContent = currentWord
+wordToGuess.textContent = (randomWord.replace(/[a-z]/g, '_'))
 
+//display 10 remaining guesses in remaining-guesses
 
-/*display 10 remaining guesses in remaining-guesses*/
+var remainingGuesses = document.getElementById('remaining-guesses');remainingGuesses.textContent = (10)
 
-var remainingGuesses = document.getElementById('remaining-guesses'); remainingGuesses.textContent = (10)
+function startGame()
+{
+  random_index = Math.floor(Math.random() * words.length);
+  randomWord = words[random_index];
+  remainingGuesses = document.getElementById('remaining-guesses');remainingGuesses.textContent = (10)
+  incorrectLetters.textContent = "";
+  alert("Word: "+randomWord);
+}
 
-/*when user guesses a letter, check whether letter is included in the word
-if yes- replace underscore with letter
-if no - incorrect guessed letter should be added to incorrect-letters
-- remaining-guesses should reflect one fewer remaining guess*/
+window.onload = function()
+{
+  alert("Word: "+randomWord);
+}
 
+//when user guesses a letter, check whether letter is included in the word
 document.onkeyup = function(e) {
-var key = e.key.toLowerCase()
-  // handle correct guess  
-
- 
-if (randomWord.includes(key)) 
- { 
-  const letters = randomWord.split('') 
-  const index = letters.findIndex(letter => letter === key)
-  const newWordArray = currentWord.split('')
-  newWordArray.splice(index, 1, e.key)
-  currentWord = newWordArray.join('')
-
-  console.log(newWordArray.join(''))
-
-  wordToGuess.textContent = currentWord
-  correct++
-  wins.textContent = correct
-  // handle incorrect guess
-} else {
-  incorrect++
-  incorrectLetters.textContent += `${e.key}`
-  remainingGuesses.textContent = (10 - incorrect) 
+let str = wordToGuess.textContent;
+//alert(wordToGuess.textContent);
+  var key = e.key.toLowerCase()
+  for(let i = 0; i < randomWord.length; i++)
+  {
+    if(key == randomWord[i])
+    {
+      str = str.substring(0,i)+key+str.substring(i+1); 
+      wordToGuess.textContent = str; 
+      correct++;
+    }
+    else if(wordToGuess.textContent.includes(key) ==false&& i == randomWord.length - 1)//At the end
+    {
+      incorrect++
+      incorrectLetters.textContent += ` ${e.key}`
+      remainingGuesses.textContent = (10 - incorrect) 
+      if(remainingGuesses.textContent ==("0"))
+      {
+        startGame();
+        loss++;
+        losses.textContent = loss;
+      }
+      break;
+    }
+    if(correct == 10)
+    {
+      win++;
+      win.textContent = "Wins:: "+win;
+      startGame();
+    }
+  }
 }
-}
+// for (var i = 0; i < randomWord.length; i++) {
+// document.onkeyup = function(e) {
+// var key = e.key.toLowerCase()
+//   // handle correct guess 
+// if (randomWord[i]==key) {//If we find a match
+//   alert("Index: "+i);
+//   // wordToGuess.textContent = randomWord.replace(randomWord, e.key)
+//   wordToGuess[i] = key;
+//   // handle incorrect guess
+// } else {
+//   alert("randomWord: "+randomWord+"\nKey: "+key);
+//   incorrect++
+//   incorrectLetters.textContent += ` ${e.key}`
+//   remainingGuesses.textContent = (10 - incorrect) 
+// }
+// }
 
+
+// }
+// when user gueses whole word correctly, wins should increase by one, next random word should be selected, and previous word should be displayed- reset all other elements
+
+
+//when remaining guesses are 0, reset to next game and losses should increase
 
 
 
